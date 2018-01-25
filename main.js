b4c9b6e15e295b589b0e715aa2d4714057a58ea4
jeffreynerona <jeffnerona@gmail.com>
2018-01-25 23:11:47 +0800

    Fixed error for testing

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ function login(event) {
         	}
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert(XMLHttpRequest);
+            alert(XMLHttpRequest.toSource());
+            alert(JSON.stringify(XMLHttpRequest, null, 4));
         }
     }); 
 }
